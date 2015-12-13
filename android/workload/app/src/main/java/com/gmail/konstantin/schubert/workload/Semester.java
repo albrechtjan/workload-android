599b7b022be10d5f69e2a8d4ccdfa27a4ff303d1
KonstantinSchubert <schubert.konstantin@gmail.com>
2015-12-13 01:04:15 +0100

    had forgotten to add semester class

---
+++ android/workload/app/src/main/java/com/gmail/konstantin/schubert/workload/Semester.java
@@ -0,0 +1,56 @@
+package com.gmail.konstantin.schubert.workload;
+
+
+public class Semester {
+
+//    to_string(), Semester(String), get_next(), get_previous()
+
+
+    private enum SemesterType {
+        WS,
+        SS;
+
+
+    };
+    private int year; //by definition the year in which the semester starts
+    private final SemesterType sSemesterType;
+
+
+    public Semester(String s){
+        if(s.contains("WS")){
+            this.sSemesterType = SemesterType.WS;
+        } else{
+            this.sSemesterType = SemesterType.SS;
+        }
+        this.year = Integer.valueOf(s.substring(2,6));
+    }
+
+    public Semester(int year, SemesterType semesterType){
+        this.sSemesterType = semesterType;
+        this.year = year;
+    }
+
+    public Semester get_next(){
+        if(this.sSemesterType.equals(SemesterType.SS)) {
+            return new Semester(this.year, SemesterType.WS);
+        } else {
+            return new Semester(this.year+1,SemesterType.SS);
+        }
+    }
+
+    public Semester get_previous(){
+        if(this.sSemesterType.equals(SemesterType.SS)){
+            return new Semester(this.year-1, SemesterType.WS);
+        }else{
+            return new Semester(this.year, SemesterType.SS);
+        }
+    }
+
+    public String to_string(){
+        if(this.sSemesterType.equals(SemesterType.SS)){
+            return "SS"+String.valueOf(year);
+        } else {
+            return "WS"+String.valueOf(year) + "/" + String.valueOf((year+1)%100);
+        }
+    }
+}
