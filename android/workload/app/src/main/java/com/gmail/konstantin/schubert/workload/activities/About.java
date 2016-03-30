2fa07a5057e990875024944f4760e4a8e3b2f3ee
KonstantinSchubert <schubert.konstantin@gmail.com>
2016-03-30 20:54:56 +0200

    drafted about activity and finished first pie chart. Second pie chart commented out currently

--- android/workload/app/src/main/java/com/gmail/konstantin/schubert/workload/activities/About.java
+++ android/workload/app/src/main/java/com/gmail/konstantin/schubert/workload/activities/About.java
@@ -2,10 +2,13 @@ package com.gmail.konstantin.schubert.workload.activities;
 
 import android.os.Bundle;
 
+import com.gmail.konstantin.schubert.workload.R;
+
 public class About extends MyBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
     }
 }
--- android/workload/app/src/main/java/com/gmail/konstantin/schubert/workload/activities/Statistics.java
+++ android/workload/app/src/main/java/com/gmail/konstantin/schubert/workload/activities/Statistics.java
@@ -2,7 +2,6 @@ package com.gmail.konstantin.schubert.workload.activities;
 
 
 import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
 
 import com.github.mikephil.charting.animation.Easing;
@@ -11,96 +10,109 @@ import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.gmail.konstantin.schubert.workload.DBObjectBuilder;
+import com.gmail.konstantin.schubert.workload.Lecture;
 import com.gmail.konstantin.schubert.workload.R;
+import com.gmail.konstantin.schubert.workload.WorkloadEntry;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Statistics extends MyBaseActivity {
 
-    protected String[] mParties = new String[] {
-            "Party A", "Party B", "Party C", "Party D", "Party E", "Party F", "Party G", "Party H",
-            "Party I", "Party J", "Party K", "Party L", "Party M", "Party N", "Party O", "Party P",
-            "Party Q", "Party R", "Party S", "Party T", "Party U", "Party V", "Party W", "Party X",
-            "Party Y", "Party Z"
-    };
+    DBObjectBuilder dbObjectBuilder;
+    List<Lecture> mActiveLectures;
+    Map<Lecture, Map<Integer,Double>> hoursSpentMap;
 
-
+    public final static int HOURS_ATTENDING = 0;
+    public final static int HOURS_HOMEWORK = 1;
+    public final static int HOURS_STUDYING = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
-        PieChart pieChart = (PieChart) findViewById(R.id.pie_chart);
-        pieChart.setUsePercentValues(true);
-        pieChart.setDescription("");
-        pieChart.setExtraOffsets(5, 10, 5, 5);
+        dbObjectBuilder = new DBObjectBuilder(getContentResolver());
 
-        pieChart.setDragDecelerationFrictionCoef(0.95f);
+        hoursSpentMap = new HashMap<>();
 
-//        pieChart.setCenterTextTypeface(Typeface.createFromAsset(getAssets(), "OpenSans-Light.ttf"));
-        pieChart.setCenterText("test");
+        collectData();
 
-        pieChart.setDrawHoleEnabled(true);
-        pieChart.setHoleColor(Color.WHITE);
+        PieChart pieChartTimePerLecture = (PieChart) findViewById(R.id.pie_chart);
+        buildPieChartTimePerLecture(pieChartTimePerLecture);
+//
+//        PieChart pieChartTimePerActivity = (PieChart) findViewById(R.id.pie_chart_time_per_activity);
+//        buildPieChartTimePerActivity(pieChartTimePerActivity);
 
-        pieChart.setTransparentCircleColor(Color.WHITE);
-        pieChart.setTransparentCircleAlpha(110);
 
-        pieChart.setHoleRadius(58f);
-        pieChart.setTransparentCircleRadius(61f);
 
-        pieChart.setDrawCenterText(true);
-
-        pieChart.setRotationAngle(0);
-        // enable rotation of the chart by touch
-        pieChart.setRotationEnabled(true);
-        pieChart.setHighlightPerTapEnabled(true);
-
-        // pieChart.setUnit(" €");
-        // pieChart.setDrawUnitsInChart(true);
-
-        // add a selection listener
-//        pieChart.setOnChartValueSelectedListener(this);
-
-        setData(pieChart);
-
-        pieChart.animateY(1400, Easing.EasingOption.EaseInOutQuad);
-        // pieChart.spin(2000, 0, 360);
-
-        Legend l = pieChart.getLegend();
-        l.setPosition(Legend.LegendPosition.RIGHT_OF_CHART);
-        l.setXEntrySpace(7f);
-        l.setYEntrySpace(0f);
-        l.setYOffset(0f);
     }
 
 
-    private void setData(PieChart pieChart) {
+
+    private void collectData(){
+
+        mActiveLectures = dbObjectBuilder.getLectureList(true);
+
+        for (Lecture lecture : mActiveLectures){
+            // query all workload entries for the lecture
+             List<WorkloadEntry> workloadEntries = dbObjectBuilder.getWorkloadEntries(lecture);
+            double homeworkHours = 0;
+            double lectureHours = 0;
+            double studyingHours = 0;
+            for(WorkloadEntry workloadEntry : workloadEntries){
+                homeworkHours += workloadEntry.getHoursForHomework();
+                lectureHours  += workloadEntry.getHoursInLecture();
+                studyingHours += workloadEntry.getHoursStudying();
+            }
+
+            Map<Integer, Double> hours = new HashMap<>();
+            hours.put(HOURS_ATTENDING, lectureHours);
+            hours.put(HOURS_STUDYING, studyingHours);
+            hours.put(HOURS_HOMEWORK, homeworkHours);
+            hoursSpentMap.put(lecture,hours);
+        }
+
+    }
 
 
-        ArrayList<Entry> yVals1 = new ArrayList<Entry>();
+    private void buildPieChartTimePerLecture(PieChart pieChartTimePerLecture) {
+
+        pieChartTimePerLecture.setExtraOffsets(5, 10, 5, 5);
+        pieChartTimePerLecture.setDragDecelerationFrictionCoef(0.95f);
+        pieChartTimePerLecture.setCenterText("Distribution of time onto lectures.");
+        pieChartTimePerLecture.setDrawHoleEnabled(true);
+        pieChartTimePerLecture.setHoleColor(Color.WHITE);
+        pieChartTimePerLecture.setTransparentCircleColor(Color.WHITE);
+        pieChartTimePerLecture.setTransparentCircleAlpha(110);
+        pieChartTimePerLecture.setHoleRadius(58f);
+        pieChartTimePerLecture.setTransparentCircleRadius(61f);
+        pieChartTimePerLecture.setDrawCenterText(true);
+        pieChartTimePerLecture.setRotationAngle(0);
+        pieChartTimePerLecture.setRotationEnabled(true);
+        pieChartTimePerLecture.setHighlightPerTapEnabled(true);
+
+
+        ArrayList<Entry> yVals1 = new ArrayList<>();
+        ArrayList<String> xVals = new ArrayList<>();
 
         // IMPORTANT: In a PieChart, no values (Entry) should have the same
         // xIndex (even if from different DataSets), since no values can be
         // drawn above each other.
 
-        yVals1.add(new Entry((float) 12, 0));
-        yVals1.add(new Entry((float) 3, 1));
-        yVals1.add(new Entry((float) 11, 2));
-        yVals1.add(new Entry((float) 7, 3));
+        int count = 0;
+        for (Lecture lecture : mActiveLectures){
+            xVals.add(lecture.name);
+            double totalHours = hoursSpentMap.get(lecture).get(HOURS_HOMEWORK) +
+                                hoursSpentMap.get(lecture).get(HOURS_STUDYING) +
+                                hoursSpentMap.get(lecture).get(HOURS_ATTENDING);
+            yVals1.add(new Entry((float) totalHours, count));
+        }
 
-
-        ArrayList<String> xVals = new ArrayList<String>();
-
-
-        xVals.add("lecture1");
-        xVals.add("lecture2");
-        xVals.add("lecture3");
-        xVals.add("lecture4");
-
-        PieDataSet dataSet = new PieDataSet(yVals1, "Election Results");
+        PieDataSet dataSet = new PieDataSet(yVals1, "");
         dataSet.setSliceSpace(3f);
         dataSet.setSelectionShift(5f);
 
@@ -115,14 +127,86 @@ public class Statistics extends MyBaseActivity {
 
 
         PieData data = new PieData(xVals, dataSet);
-        data.setValueFormatter(new PercentFormatter());
         data.setValueTextSize(11f);
-        data.setValueTextColor(Color.WHITE);
-        pieChart.setData(data);
+        data.setValueTextColor(Color.BLACK);
+        pieChartTimePerLecture.setData(data);
 
-        pieChart.highlightValues(null);
+        pieChartTimePerLecture.highlightValues(null);
 
-        pieChart.invalidate();
+        pieChartTimePerLecture.invalidate();
+
+        pieChartTimePerLecture.animateY(1400, Easing.EasingOption.EaseInOutQuad);
+        Legend l = pieChartTimePerLecture.getLegend();
+        l.setPosition(Legend.LegendPosition.RIGHT_OF_CHART);
+        l.setXEntrySpace(7f);
+        l.setYEntrySpace(0f);
+        l.setYOffset(0f);
     }
+
+//
+//    private void buildPieChartTimePerActivity(PieChart pieChart) {
+//
+//        pieChart.setExtraOffsets(5, 10, 5, 5);
+//        pieChart.setDragDecelerationFrictionCoef(0.95f);
+//        pieChart.setCenterText("Distribution of time onto lectures.");
+//        pieChart.setDrawHoleEnabled(true);
+//        pieChart.setHoleColor(Color.WHITE);
+//        pieChart.setTransparentCircleColor(Color.WHITE);
+//        pieChart.setTransparentCircleAlpha(110);
+//        pieChart.setHoleRadius(58f);
+//        pieChart.setTransparentCircleRadius(61f);
+//        pieChart.setDrawCenterText(true);
+//        pieChart.setRotationAngle(0);
+//        pieChart.setRotationEnabled(true);
+//        pieChart.setHighlightPerTapEnabled(true);
+//
+//
+//        ArrayList<Entry> yVals1 = new ArrayList<>();
+//        ArrayList<String> xVals = new ArrayList<>();
+//
+//        // IMPORTANT: In a PieChart, no values (Entry) should have the same
+//        // xIndex (even if from different DataSets), since no values can be
+//        // drawn above each other.
+//
+//        int count = 0;
+//        for (Lecture lecture : mActiveLectures){
+//            xVals.add(lecture.name);
+//            double totalHours = hoursSpentMap.get(lecture).get(HOURS_HOMEWORK) +
+//                    hoursSpentMap.get(lecture).get(HOURS_STUDYING) +
+//                    hoursSpentMap.get(lecture).get(HOURS_ATTENDING);
+//            yVals1.add(new Entry((float) totalHours, count));
+//        }
+//
+//        PieDataSet dataSet = new PieDataSet(yVals1, "");
+//        dataSet.setSliceSpace(3f);
+//        dataSet.setSelectionShift(5f);
+//
+//        ArrayList<Integer> colors = new ArrayList<Integer>();
+//
+//        for (int c : ColorTemplate.JOYFUL_COLORS)
+//            colors.add(c);
+//
+//        colors.add(ColorTemplate.getHoloBlue());
+//
+//        dataSet.setColors(colors);
+//
+//
+//        PieData data = new PieData(xVals, dataSet);
+//        data.setValueTextSize(11f);
+//        data.setValueTextColor(Color.BLACK);
+//        pieChart.setData(data);
+//
+//        pieChart.highlightValues(null);
+//
+//        pieChart.invalidate();
+//
+//        pieChart.animateY(1400, Easing.EasingOption.EaseInOutQuad);
+//        Legend l = pieChart.getLegend();
+//        l.setPosition(Legend.LegendPosition.RIGHT_OF_CHART);
+//        l.setXEntrySpace(7f);
+//        l.setYEntrySpace(0f);
+//        l.setYOffset(0f);
+//    }
+
 
 }
