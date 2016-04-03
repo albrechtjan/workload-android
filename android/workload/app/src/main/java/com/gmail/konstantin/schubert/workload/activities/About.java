47c7f94b0bc6840a49cd1fba96a41c79d4c8980c
Konstantin Schubert <schubert.konstantin@gmail.com>
2016-04-03 19:24:26 +0200

    added mailto link. Somehow can't get the web link to my homepage to work. Oh well.

--- android/workload/app/src/main/java/com/gmail/konstantin/schubert/workload/activities/About.java
+++ android/workload/app/src/main/java/com/gmail/konstantin/schubert/workload/activities/About.java
@@ -1,6 +1,10 @@
 package com.gmail.konstantin.schubert.workload.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 import com.gmail.konstantin.schubert.workload.R;
 
@@ -10,5 +14,15 @@ public class About extends MyBaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+
+        TextView feedback = (TextView) findViewById(R.id.text_email);
+        feedback.setText(Html.fromHtml(getString(R.string.about_email)));
+        feedback.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView about = (TextView) findViewById(R.id.text_about);
+        about.setText(Html.fromHtml(getString(R.string.about)));
+        about.setMovementMethod(LinkMovementMethod.getInstance());
+
     }
 }
